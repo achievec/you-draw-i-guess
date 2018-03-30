bf8067d0698f454df3faef3dbb09bcc67db6d031
achievec <achievec@gmail.com>
2018-03-30 11:40:44 +0800

    init project

---
+++ src/test/java/org/aloha/youdrawiguess/YouDrawIGuessApplicationTests.java
@@ -0,0 +1,16 @@
+package org.aloha.youdrawiguess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YouDrawIGuessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
